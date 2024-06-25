517f01adae6cff6068a0622d7f9898c5c02401e4
Your_Name Your_Surname <your@email>
2024-06-25 19:36:03 +0300

    Create view by register, login and logout endpoints

--- user/views.py
+++ user/views.py
@@ -1,3 +1,37 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.contrib.auth import authenticate, login, logout
+from .serializers import UserSerializer, RegisterUserSerializer, LoginSerializer
 
 
+class RegisterAPIView(generics.GenericAPIView):
+    serializer_class = RegisterUserSerializer
 
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        })
+
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = authenticate(email=serializer.validated_data["email"], password=serializer.validated_data["password"])
+        if user is not None:
+            login(request, user)
+            return Response({
+                "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            })
+        return Response({"error": "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutAPIView(generics.GenericAPIView):
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
