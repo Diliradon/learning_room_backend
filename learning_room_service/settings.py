dd373f4ea996b9c1414f0422cfcc319996f72c58
Your_Name Your_Surname <your@email>
2024-06-25 12:28:55 +0300

    Add settings by authentication to settings.py

--- learning_room_service/settings.py
+++ learning_room_service/settings.py
@@ -130,3 +130,13 @@ MEDIA_ROOT = "/vol/web/media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
