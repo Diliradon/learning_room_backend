a0ed955bfa12bb761dd4c5ced63576aada5f81af
Your_Name Your_Surname <your@email>
2024-07-13 16:55:31 +0300

    Change signal by generate unique_key

--- course_service/signals.py
+++ course_service/signals.py
@@ -7,4 +7,4 @@ from course_service.models import Course
 @receiver(pre_save, sender=Course)
 def add_unique_key(sender, instance, **kwargs):
     if not instance.unique_key:
-        instance.unique_key = shortuuid.ShortUUID.random(length=6)
+        instance.unique_key = shortuuid.ShortUUID().random(length=6)
