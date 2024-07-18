af0f32461287fbd57ae7ee984e57504bfee24ba4
Your_Name Your_Surname <your@email>
2024-07-18 14:54:28 +0300

    Change method Task

--- task_service/models.py
+++ task_service/models.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from learning_room_service.settings import AUTH_USER_MODEL
+from course_service.models import Course
 
 
 class Task(models.Model):
@@ -20,17 +22,29 @@ class Task(models.Model):
         (1, "All students"),
         (2, "Select students"),
     )
-    type_of_task = models.CharField(choices=CHOICES_TYPE_OF_TASK, blank=False, )
+    type_of_task = models.IntegerField(choices=CHOICES_TYPE_OF_TASK, blank=False, null=False)
     topic = models.CharField(max_length=150, null=False, blank=False)
     additionally = models.TextField(blank=True, null=False)
     answer_link = models.URLField(max_length=200, null=False, blank=True)
     answer_file = models.FileField(upload_to="uploads/", null=False, blank=True)
     answer_image = models.ImageField(upload_to="images/", null=False, blank=True)
-    rating = models.CharField(choices=CHOICES_RATING, blank=False, null=False)
-    for_whom = models.CharField(choices=CHOICES_FOR_WHOM, blank=False, null=False)
+    rating = models.IntegerField(choices=CHOICES_RATING, blank=False, null=False)
+    for_whom = models.IntegerField(choices=CHOICES_FOR_WHOM, blank=False, null=False)
     students = models.ManyToManyField(
         AUTH_USER_MODEL,
         blank=True,
-        null=False,
         related_name="doing_courses"
     )
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="tasks")
+    deadline = models.DateTimeField(null=True, blank=True)
+
+    def clean(self):
+        if self.for_whom == 1 and self.students.exists():
+            raise ValidationError("You cannot select students when 'for_whom' is set to 'All students'.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.topic
