ab7e000fdf551b8f14be5743c56fd6ed4485bf0b
Your_Name Your_Surname <your@email>
2024-07-19 17:44:35 +0300

    Delete pywin from requirements.txt and create new path by tasks

--- task_service/urls.py
+++ task_service/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested import routers
 from task_service.views import (
     TeachingTaskViewSet,
 )
+from course_service.urls import router
 
-router = routers.DefaultRouter()
-router.register("teaching-tasks", TeachingTaskViewSet, basename="teaching-tasks")
+course_router = routers.NestedDefaultRouter(router, r"teaching-courses", lookup="course")
+course_router.register("teaching-tasks", TeachingTaskViewSet, basename="teaching-tasks")
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(course_router.urls))
 ]
-
 
 app_name = "task-service"
