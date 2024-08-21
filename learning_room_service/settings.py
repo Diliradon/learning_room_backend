1d905f8f5fa008cf466bf0749a8fb9f0ae239467
Your_Name Your_Surname <your@email>
2024-08-21 12:03:30 +0300

    Add CORS_ALLOW_ALL_ORIGIN to setting.py

--- learning_room_service/settings.py
+++ learning_room_service/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     "user",
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
